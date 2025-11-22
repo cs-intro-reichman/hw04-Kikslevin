c42af049488cb59af17f05c1e71d11d318c9d582
Kikslevin <kira.levin@post.runi.ac.il>
2025-11-22 19:26:47 +0200

    Completed assignment

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -169,8 +169,7 @@ public class ArrCharOps {
      *         return -2 if there is an error with the input.
      */
     public static int compareTo(String str1, String str2) {
-        if (str1 == null || str2 == null) return -2;
-        
+
         int minlength = Math.min(str1.length(), str2.length());
 
         for (int i = 0; i < minlength; i++) {
